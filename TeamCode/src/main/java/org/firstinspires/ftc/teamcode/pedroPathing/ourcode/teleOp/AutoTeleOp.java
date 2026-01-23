8d8ae5eddbe2a28a29b8525fca6cf1647c19bb5e
D01-5irobotics <D01@5irobotics.org>
2026-01-22 21:43:11 -0600

    Meet 3, great code but not really working, time pls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ourcode/teleOp/AutoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ourcode/teleOp/AutoTeleOp.java
@@ -1,267 +1,232 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.ourcode.teleOp;
 
-import com.pedropathing.telemetry.SelectableOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "HI HI HI HI HI", group = "Z")
-public class AutoTeleOp extends SelectableOpMode {
+@TeleOp(name="AUTO + TELEOP MENU", group="Z")
+public class AutoTeleOp extends OpMode {
 
-    public AutoTeleOp() {
-        super("Select Mode", menu -> {
-            menu.add("TeleOp Only",
-                    () -> new DriveMode("NONE", false));
+    enum Mode { TELEOP, AUTO_TELEOP, AUTO }
+    enum Alliance { RED, BLUE }
+    enum Path { SHORT, LONG }
 
-            menu.folder("Auto + TeleOp", m -> {
-                m.add("Red Short (24in)", () -> new DriveMode("RED_SHORT", true));
-                m.add("Blue Short (24in)", () -> new DriveMode("BLUE_SHORT", true));
-                m.add("Test Turn (90deg)", () -> new DriveMode("TEST_TURN", true));
-            });
+    // ======== Menu selections ========
+    Mode selectedMode = Mode.TELEOP;
+    Alliance selectedAlliance = Alliance.RED;
+    Path selectedPath = Path.SHORT;
+    int ballRows = 1;
 
-            menu.folder("Auto Only", m -> {
-                m.add("Red Short", () -> new DriveMode("RED_SHORT", false));
-                m.add("Blue Short", () -> new DriveMode("BLUE_SHORT", false));
-            });
-        });
+    // ======== Robot hardware ========
+    private DcMotor FLeft, BLeft, FRight, BRight;
+    private DcMotor IntakeMotor;
+    private DcMotorEx Shooter;
+    private CRServo SmallSupportServo, LargeSupportServo, Ramp;
+
+    private ElapsedTime autoTimer = new ElapsedTime();
+    private boolean autoDriving = false;
+    private int autoStep = 0;
+
+    // ======== Button edge detection ========
+    private ButtonEdge dpadUp = new ButtonEdge();
+    private ButtonEdge dpadDown = new ButtonEdge();
+    private ButtonEdge dpadLeft = new ButtonEdge();
+    private ButtonEdge dpadRight = new ButtonEdge();
+    private ButtonEdge aButton = new ButtonEdge();
+    private ButtonEdge bButton = new ButtonEdge();
+    private ButtonEdge xButton = new ButtonEdge();
+    private ButtonEdge yButton = new ButtonEdge();
+
+    @Override
+    public void init() {
+        // Motors
+        FLeft = hardwareMap.get(DcMotor.class, "FLeft");
+        BLeft = hardwareMap.get(DcMotor.class, "BLeft");
+        FRight = hardwareMap.get(DcMotor.class, "FRight");
+        BRight = hardwareMap.get(DcMotor.class, "BRight");
+
+        IntakeMotor = hardwareMap.get(DcMotor.class, "Intake");
+        Shooter = hardwareMap.get(DcMotorEx.class, "Shooter");
+        SmallSupportServo = hardwareMap.get(CRServo.class, "SmallSupportServo");
+        LargeSupportServo = hardwareMap.get(CRServo.class, "LargeSupportServo");
+        Ramp = hardwareMap.get(CRServo.class, "Ramp");
+
+        // Motor directions
+        FLeft.setDirection(DcMotor.Direction.REVERSE);
+        BLeft.setDirection(DcMotor.Direction.REVERSE);
+        FRight.setDirection(DcMotor.Direction.FORWARD);
+        BRight.setDirection(DcMotor.Direction.FORWARD);
+        Shooter.setDirection(DcMotor.Direction.REVERSE);
+
+        setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addLine("Initialized! Use D-Pad + Buttons to select options.");
+        telemetry.update();
     }
 
     @Override
-    public void onSelect() {}
+    public void loop() {
 
-    /* ===================== DRIVE MODE ===================== */
-
-    public static class DriveMode extends OpMode {
-
-        double F = 0.8592;
-        double P = 5.9890;
-
-        private final String autoType;
-        private final boolean goTeleOpAfterAuto;
-
-        private boolean autoStarted = false;
-        private boolean autoFinished = false;
-        private boolean autoDriving = false;
-
-        private DcMotor IntakeMotor, FLeft, BLeft, FRight, BRight;
-        private DcMotorEx Shooter;
-        private CRServo SmallSupportServo, LargeSupportServo, Ramp;
-
-        private final ButtonEdge shooterHigh = new ButtonEdge();
-        private final ButtonEdge shooterLow  = new ButtonEdge();
-        private final ButtonEdge shooterOff  = new ButtonEdge();
-        private final ButtonEdge rampOut     = new ButtonEdge();
-        private final ButtonEdge rampIn      = new ButtonEdge();
-
-        public DriveMode(String autoType, boolean goTeleOpAfterAuto) {
-            this.autoType = autoType;
-            this.goTeleOpAfterAuto = goTeleOpAfterAuto;
+        // ======= Menu selection (before start) =======
+        if (false) {
+            handleMenuSelection();
+            return;
         }
 
-        @Override
-        public void init() {
-
-            IntakeMotor = hardwareMap.get(DcMotor.class, "Intake");
-            Shooter = hardwareMap.get(DcMotorEx.class, "Shooter");
-            SmallSupportServo = hardwareMap.get(CRServo.class, "SmallSupportServo");
-            LargeSupportServo = hardwareMap.get(CRServo.class, "LargeSupportServo");
-            Ramp = hardwareMap.get(CRServo.class, "Ramp");
-
-            FLeft = hardwareMap.get(DcMotor.class, "FLeft");
-            BLeft = hardwareMap.get(DcMotor.class, "BLeft");
-            FRight = hardwareMap.get(DcMotor.class, "FRight");
-            BRight = hardwareMap.get(DcMotor.class, "BRight");
-
-            SmallSupportServo.setDirection(DcMotorSimple.Direction.REVERSE);
-            FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-            BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-            Shooter.setDirection(DcMotorSimple.Direction.REVERSE);
-
-            setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            Shooter.setPIDFCoefficients(
-                    DcMotor.RunMode.RUN_USING_ENCODER,
-                    new PIDFCoefficients(P, 0, 0, F)
-            );
-
-            telemetry.addLine("Initialized");
-            telemetry.addData("Auto Mode", autoType);
-            telemetry.update();
-        }
-
-        @Override
-        public void loop() {
-
-            /* ================= TELEMETRY (GLOBAL) ================= */
-            telemetry.addData("Auto Mode", autoType);
-            telemetry.addData("Auto Started", autoStarted);
-            telemetry.addData("Auto Finished", autoFinished);
-            telemetry.addData("Auto Driving", autoDriving);
-
-            telemetry.addData("FL Encoder", FLeft.getCurrentPosition());
-            telemetry.addData("FR Encoder", FRight.getCurrentPosition());
-            telemetry.addData("BL Encoder", BLeft.getCurrentPosition());
-            telemetry.addData("BR Encoder", BRight.getCurrentPosition());
-
-            telemetry.addData("FL Busy", FLeft.isBusy());
-            telemetry.addData("FR Busy", FRight.isBusy());
-            telemetry.addData("BL Busy", BLeft.isBusy());
-            telemetry.addData("BR Busy", BRight.isBusy());
-
-            /* ================= AUTO MOTION HANDLER ================= */
-            if (autoDriving) {
-
-                telemetry.addLine("AUTO: Driving to target");
-
-                if (!FLeft.isBusy() && !FRight.isBusy()
-                        && !BLeft.isBusy() && !BRight.isBusy()) {
-
-                    stopDrivetrain();
-                    setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-                    autoDriving = false;
-                    autoFinished = true;
-
-                    telemetry.addLine("AUTO: Finished movement");
-
-                    if (!goTeleOpAfterAuto) {
-                        telemetry.addLine("AUTO ONLY: Stopping OpMode");
-                        telemetry.update();
-                        requestOpModeStop();
-                    }
+        // ======= Main robot logic =======
+        switch (selectedMode) {
+            case TELEOP:
+                teleOpLoop();
+                break;
+            case AUTO_TELEOP:
+            case AUTO:
+                autoLoop();
+                if (selectedMode == Mode.AUTO_TELEOP) {
+                    teleOpLoop();
                 }
-
-                telemetry.update();
-                return;
-            }
-
-            /* ================= AUTO START ================= */
-            if (!autoType.equals("NONE") && !autoFinished && !autoStarted) {
-                telemetry.addLine("AUTO: Starting");
-                runSelectedAuto();
-                autoStarted = true;
-                telemetry.update();
-                return;
-            }
-
-            /* ================= TELEOP ================= */
-            telemetry.addLine("TELEOP: Active");
-
-            double drive = -gamepad1.left_stick_y;
-            double strafe = gamepad1.left_stick_x;
-            double turn = gamepad1.right_stick_x;
-
-            FLeft.setPower((drive + strafe + turn) * Constants.DRIVE_SPEED_MULT);
-            BLeft.setPower((drive - strafe + turn) * Constants.DRIVE_SPEED_MULT);
-            FRight.setPower((drive - strafe - turn) * Constants.DRIVE_SPEED_MULT);
-            BRight.setPower((drive + strafe - turn) * Constants.DRIVE_SPEED_MULT);
-
-            if (shooterHigh.wasPressed(gamepad2.y)) {
-                Shooter.setVelocity(Constants.SHOOTER_HIGH_RPM);
-                telemetry.addLine("Shooter: HIGH");
-            }
-
-            if (shooterLow.wasPressed(gamepad2.b)) {
-                Shooter.setVelocity(Constants.SHOOTER_LOW_RPM);
-                telemetry.addLine("Shooter: LOW");
-            }
-
-            if (shooterOff.wasPressed(gamepad2.x)) {
-                Shooter.setPower(0);
-                telemetry.addLine("Shooter: OFF");
-            }
-
-            if (rampOut.wasPressed(gamepad2.dpad_down))
-                Ramp.setPower(Constants.RAMP_OUT);
-
-            if (rampIn.wasPressed(gamepad2.dpad_up))
-                Ramp.setPower(Constants.RAMP_IN);
-
-            if (!gamepad2.dpad_down && !gamepad2.dpad_up)
-                Ramp.setPower(0);
-
-            IntakeMotor.setPower(-gamepad2.right_stick_y * Constants.INTAKE_SPEED);
-
-            double helperPower = -gamepad2.left_stick_y * Constants.HELPER_WHEEL_SPEED;
-            SmallSupportServo.setPower(helperPower);
-            LargeSupportServo.setPower(helperPower);
-
-            telemetry.update();
+                break;
         }
 
-        /* ================= AUTO ================= */
+        // Telemetry
+        telemetry.addLine("Mode: " + selectedMode);
+        telemetry.addLine("Alliance: " + selectedAlliance);
+        telemetry.addLine("Path: " + selectedPath);
+        telemetry.addLine("Ball Rows: " + ballRows);
+        telemetry.update();
+    }
 
-        private void runSelectedAuto() {
-            telemetry.addData("AUTO COMMAND", autoType);
-
-            switch (autoType) {
-                case "RED_SHORT":
-                case "BLUE_SHORT":
-                    encoderDriving(0.5, 24, 24);
-                    break;
-
-                case "TEST_TURN":
-                    encoderTurn(0.4, 90);
-                    break;
-            }
+    private void handleMenuSelection() {
+        // Mode selection
+        if (dpadUp.wasPressed(gamepad1.dpad_up)) {
+            selectedMode = Mode.values()[(selectedMode.ordinal() + 1) % Mode.values().length];
+        }
+        if (dpadDown.wasPressed(gamepad1.dpad_down)) {
+            selectedMode = Mode.values()[(selectedMode.ordinal() - 1 + Mode.values().length) % Mode.values().length];
         }
 
-        public void encoderTurn(double speed, double degrees) {
-            double turnDist =
-                    Math.PI * Constants.TRACK_WIDTH_INCHES * (Math.abs(degrees) / 360.0);
-
-            double left = degrees > 0 ? turnDist : -turnDist;
-            double right = -left;
-
-            telemetry.addData("Turn Degrees", degrees);
-            encoderDriving(speed, left, right);
+        // Alliance selection
+        if (dpadLeft.wasPressed(gamepad1.dpad_left)) {
+            selectedAlliance = selectedAlliance == Alliance.RED ? Alliance.BLUE : Alliance.RED;
         }
 
-        public void encoderDriving(double speed, double leftInches, double rightInches) {
-
-            int leftTicks = (int) (leftInches * Constants.COUNTS_PER_INCH);
-            int rightTicks = (int) (rightInches * Constants.COUNTS_PER_INCH);
-
-            telemetry.addData("Left Inches", leftInches);
-            telemetry.addData("Right Inches", rightInches);
-            telemetry.addData("Left Ticks", leftTicks);
-            telemetry.addData("Right Ticks", rightTicks);
-
-            FLeft.setTargetPosition(FLeft.getCurrentPosition() + leftTicks);
-            BLeft.setTargetPosition(BLeft.getCurrentPosition() + leftTicks);
-            FRight.setTargetPosition(FRight.getCurrentPosition() + rightTicks);
-            BRight.setTargetPosition(BRight.getCurrentPosition() + rightTicks);
-
-            setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            FLeft.setPower(speed);
-            BLeft.setPower(speed);
-            FRight.setPower(speed);
-            BRight.setPower(speed);
-
-            autoDriving = true;
+        // Path selection
+        if (dpadRight.wasPressed(gamepad1.dpad_right)) {
+            selectedPath = selectedPath == Path.SHORT ? Path.LONG : Path.SHORT;
         }
 
-        private void stopDrivetrain() {
-            FLeft.setPower(0);
-            BLeft.setPower(0);
-            FRight.setPower(0);
-            BRight.setPower(0);
-        }
+        // Ball rows selection
+        if (aButton.wasPressed(gamepad1.a)) ballRows = Math.max(1, ballRows - 1);
+        if (bButton.wasPressed(gamepad1.b)) ballRows = Math.min(5, ballRows + 1);
 
-        private void setDriveMode(DcMotor.RunMode mode) {
-            FLeft.setMode(mode);
-            BLeft.setMode(mode);
-            FRight.setMode(mode);
-            BRight.setMode(mode);
+        telemetry.addLine("=== MENU SELECTION ===");
+        telemetry.addData("Mode (DPad Up/Down)", selectedMode);
+        telemetry.addData("Alliance (DPad Left)", selectedAlliance);
+        telemetry.addData("Path (DPad Right)", selectedPath);
+        telemetry.addData("Ball Rows (A/B)", ballRows);
+        telemetry.addLine("Press START to begin");
+        telemetry.update();
+    }
+
+    // ======= TeleOp =======
+    private void teleOpLoop() {
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        FLeft.setPower(drive + strafe + turn);
+        BLeft.setPower(drive - strafe + turn);
+        FRight.setPower(drive - strafe - turn);
+        BRight.setPower(drive + strafe - turn);
+
+        IntakeMotor.setPower(gamepad2.left_stick_y);
+        SmallSupportServo.setPower(gamepad2.right_stick_y);
+        LargeSupportServo.setPower(gamepad2.right_stick_y);
+
+        if (xButton.wasPressed(gamepad2.x)) shootBalls(ballRows);
+    }
+
+    // ======= Auto =======
+    private void autoLoop() {
+        switch (autoStep) {
+            case 0:
+                // Drive forward depending on path
+                double distance = selectedPath == Path.SHORT ? 24 : 48;
+                encoderDrive(0.5, distance, distance);
+                autoTimer.reset();
+                autoStep++;
+                break;
+
+            case 1:
+                if (!FLeft.isBusy()) {
+                    // Shoot balls
+                    Shooter.setVelocity(2000);
+                    SmallSupportServo.setPower(1.0);
+                    LargeSupportServo.setPower(1.0);
+                    autoTimer.reset();
+                    autoStep++;
+                }
+                break;
+
+            case 2:
+                if (autoTimer.seconds() > 5) {
+                    SmallSupportServo.setPower(0);
+                    LargeSupportServo.setPower(0);
+                    autoStep++;
+                }
+                break;
+
+            default:
+                stopDrive();
+                break;
         }
     }
 
-    /* ================= UTILS ================= */
+    // ======= Drive helpers =======
+    private void encoderDrive(double speed, double leftInches, double rightInches) {
+        int leftTicks = (int) (leftInches * Constants.COUNTS_PER_INCH);
+        int rightTicks = (int) (rightInches * Constants.COUNTS_PER_INCH);
 
+        FLeft.setTargetPosition(FLeft.getCurrentPosition() + leftTicks);
+        BLeft.setTargetPosition(BLeft.getCurrentPosition() + leftTicks);
+        FRight.setTargetPosition(FRight.getCurrentPosition() + rightTicks);
+        BRight.setTargetPosition(BRight.getCurrentPosition() + rightTicks);
+
+        setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        FLeft.setPower(speed);
+        BLeft.setPower(speed);
+        FRight.setPower(speed);
+        BRight.setPower(speed);
+
+        autoDriving = true;
+    }
+
+    private void stopDrive() {
+        FLeft.setPower(0);
+        BLeft.setPower(0);
+        FRight.setPower(0);
+        BRight.setPower(0);
+    }
+
+    private void setDriveMode(DcMotor.RunMode mode) {
+        FLeft.setMode(mode);
+        BLeft.setMode(mode);
+        FRight.setMode(mode);
+        BRight.setMode(mode);
+    }
+
+    private void shootBalls(int numBalls) {
+        SmallSupportServo.setPower(1.0);
+        LargeSupportServo.setPower(1.0);
+        // Shooting handled in autoLoop or teleOp manually
+    }
+
+    // ======= ButtonEdge helper =======
     static class ButtonEdge {
-        boolean last;
+        boolean last = false;
         boolean wasPressed(boolean current) {
             boolean pressed = current && !last;
             last = current;
@@ -270,20 +235,8 @@ public class AutoTeleOp extends SelectableOpMode {
     }
 
     public static class Constants {
-        public static double DRIVE_SPEED_MULT = 1.0;
-        public static double INTAKE_SPEED = 1.0;
-        public static double HELPER_WHEEL_SPEED = 1.0;
-        public static double RAMP_OUT = 1.0;
-        public static double RAMP_IN = -1.0;
-
-        public static double SHOOTER_HIGH_RPM = 2250;
-        public static double SHOOTER_LOW_RPM = 1925;
-
         public static final double TICKS_PER_REV = 537.7;
         public static final double WHEEL_DIAMETER = 3.77953;
-        public static final double COUNTS_PER_INCH =
-                TICKS_PER_REV / (WHEEL_DIAMETER * Math.PI);
-
-        public static final double TRACK_WIDTH_INCHES = 14.5;
+        public static final double COUNTS_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER * Math.PI);
     }
 }
